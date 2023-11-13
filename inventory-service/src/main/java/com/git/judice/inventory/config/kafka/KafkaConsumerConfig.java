bd45a995dd89089d7bb0cd4e65095291ba08bba6
Judice <r_judice@msn.com>
2023-11-13 08:07:30 -0300

    Inventory-service ajustes no GROUP_ID_CONFIG

--- inventory-service/src/main/java/com/git/judice/inventory/config/kafka/KafkaConsumerConfig.java
+++ inventory-service/src/main/java/com/git/judice/inventory/config/kafka/KafkaConsumerConfig.java
@@ -7,8 +7,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-//import org.springframework.kafka.support.serializer.JsonDeserializer;
-
 import com.git.judice.inventory.adapters.out.message.SaleMessage;
 
 import java.util.HashMap;
@@ -25,12 +23,10 @@ public class KafkaConsumerConfig {
 
     Map<String, Object> props = new HashMap<>();
     props.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(GROUP_ID_CONFIG, "inventory");
     props.put(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-    // props.put(VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
     props.put(VALUE_DESERIALIZER_CLASS_CONFIG, CustomDeserializer.class);
-
     props.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
-    // props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.git.judice.*");
     return new DefaultKafkaConsumerFactory<>(props);
 
   }
