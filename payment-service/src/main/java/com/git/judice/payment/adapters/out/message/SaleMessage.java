e681535886c84b79fb5c6161b90714ed3e910d8b
Judice <r_judice@msn.com>
2023-11-13 08:03:33 -0300

    Payment-serve funcionando

--- payment-service/src/main/java/com/git/judice/payment/adapters/out/message/SaleMessage.java
+++ payment-service/src/main/java/com/git/judice/payment/adapters/out/message/SaleMessage.java
@@ -4,16 +4,16 @@ import com.git.judice.payment.application.core.domain.Sale;
 import com.git.judice.payment.application.core.domain.enums.SaleEvent;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaleMessage {
 
-    private Sale sale;
-
-    private SaleEvent event;
-
+  private Sale sale;
+  private SaleEvent event;
 }
